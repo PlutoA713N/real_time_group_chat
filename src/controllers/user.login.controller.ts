fafdd7eafeea06ad4dd3a6debc7e1e27c7e7a726
PlutoA713N <mallachandu99@gmail.com>
2025-05-15 01:08:16 +0530

    feat: implement login controller

---
+++ src/controllers/user.login.controller.ts
@@ -0,0 +1,49 @@
+import { Request, Response, NextFunction } from "express";
+import { createError } from "./../errors/createError"; 
+import { findUserByField } from "../utils/validation.utils";
+import { comparePassword } from "./../utils/auth";
+import { error } from "console";
+import { jwtSign } from "../utils/jwt";
+import { createSuccessResponse } from "../errors/createSuccessResponse";
+
+export async function handleUserLogin(req: Request, res: Response, next: NextFunction) {
+    try {
+        const { email, username, password } = req.body;
+
+        const formData = {
+            username, email
+        }
+
+        const user = await findUserByField(formData)
+
+        if (!user) {
+            return next(createError('Invalid Credentials', 401, 'INVALID_CREDENTIALS'))
+        }
+
+        const isPasswordMatch = await comparePassword(password, user?.password)
+
+        if (!isPasswordMatch) {
+            return next(createError('Password is incorrect', 401, 'INVALID_PASSWORD'))
+        }
+
+        const payload = { 
+            userId: user._id.toString(),
+            username: user.username,
+            email: user.email,
+            createdAt: user.createdAt
+         }
+        
+        const token = jwtSign(payload, '30d')
+
+         res.status(200).json(createSuccessResponse('User logged in successfully', {
+            token: token,
+            userId: user._id.toString()
+         }))
+        
+         return
+
+    } catch (err) {
+        error('Error caught in User login:', err)
+        next(err)
+    }
+}
