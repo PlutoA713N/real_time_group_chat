031cf16de6dcd0d7efb0450080750b40eb4bc712
PlutoA713N <mallachandu99@gmail.com>
2025-05-15 01:26:17 +0530

    feat: implement user routes

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,9 +1,117 @@
-import {Router} from "express";
-import { registerUser } from "./../controllers/user.controller";
-import { validateRegistrationRules, validateRegistration } from "../middleware/validateRegistration";
+import { Router } from "express";
+import { registerUser } from "../controllers/user.registration.controller";
+import { validateRegistrationRules, validateLoginRules, validateResult, validateMessageRules } from "../middleware/validateRules";
+import { handleUserLogin } from "../controllers/user.login.controller";
+import { authenticationHandler } from "../middleware/authenticationHandler";
+import {userMessageController} from "../controllers/user.message.controller";
+import {checkidHandler} from "../middleware/checkidHandler";
 
 const router = Router();
 
-router.post("/register", validateRegistrationRules, validateRegistration, registerUser);
+/**
+ * @swagger
+ * /user/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: apple
+ *               email:
+ *                 type: string
+ *                 example: apple@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: Apple123#
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       409:
+ *         description: Username or email already exists
+ *       500:
+ *         description: Server error
+ */
+router.post("/register", validateRegistrationRules, validateResult, registerUser);
+
+/**
+ * @swagger
+ * /user/login:
+ *   post:
+ *     summary: Log in a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - email
+ *               - password
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: apple@gmail.com
+ *               password:
+ *                 type: string
+ *                 example: Apple123#
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *       401:
+ *         description: Invalid credentials
+ */
+router.post("/login", validateLoginRules, validateResult, handleUserLogin)
+
+/**
+ * @swagger
+ * /user/api/messages:
+ *   post:
+ *     summary: Send a message to user or group
+ *     tags: [Messages]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - content
+ *             properties:
+ *               senderId:
+ *                 type: string
+ *                 example: 6824c3777fa6a0a2a410c0fd
+ *               receiverId:
+ *                 type: string
+ *                 example: 68248f55f1b41aeceab2288e
+ *               groupId:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *                 example: Hello, world!
+ *     responses:
+ *       200:
+ *         description: Message sent successfully
+ *       400:
+ *         description: Bad Request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Group not found
+ */
+router.post("/api/messages", authenticationHandler, validateMessageRules, validateResult, checkidHandler, userMessageController)
 
 export default router
