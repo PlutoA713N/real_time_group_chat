6c4b36c21a12b2938c5fc6d8351f1dc85118441f
PlutoA713N <mallachandu99@gmail.com>
2025-05-15 01:27:59 +0530

    feat: implement database operations

--- src/models/dbOperations.ts
+++ src/models/dbOperations.ts
@@ -1,18 +1,19 @@
-import UserRegistrationModel from "./user.model";
+import { UserRegistrationModel } from "./user.model";
 import { AppError } from "./../errors/AppError";
 
-interface CheckResult {
+interface CheckResult <T> {
     message: string,
+    user: T | null,
     isExists: boolean
 }
 
-async function checkFieldExists(field: 'username' | 'email', value: string): Promise<CheckResult> {
+export async function checkFieldExists<T = any>(field: string, value: string): Promise<CheckResult<T>> {
     try {
-        const existingUser = await UserRegistrationModel.findOne({ [field]: value })
+        const existingUser = await UserRegistrationModel.findOne({ [field]: value }) 
         if (existingUser) {
-            return { message: `${[field]} already exists`, isExists: true }
+            return { message: `${[field]} exists`, user: existingUser as T, isExists: true }
         }
-        return { message: '', isExists: false }
+        return { message: '', user: null, isExists: false }
     } catch (error) {
         throw new AppError(
             `Failed to check ${field}: ${error instanceof Error ? error.message : String(error)}`,
@@ -22,4 +23,3 @@ async function checkFieldExists(field: 'username' | 'email', value: string): Pro
     }
 }
 
-export { checkFieldExists }
