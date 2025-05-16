2d4ec817597f2b94dbd3d8dd09304ecbacef6cb7
PlutoA713N <mallachandu99@gmail.com>
2025-05-17 02:34:48 +0530

    refactor: Allow cors origin for all

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,15 +1,23 @@
 import express, { Express } from "express"
+import cors from "cors"
 import userRoute from "../routes/user.route";
+import userMessagesRoute from "../routes/user.messages.route";
 import {errorHandler} from "../middleware/errorHandler";
 import {setupSwaggerDocs} from "./../swagger/swagger"
 
 const app: Express = express()
+
+app.use(cors({
+    origin: '*',
+    credentials: true,
+}))
 
 app.use(express.json())
 
 setupSwaggerDocs(app)
 
 app.use('/user', userRoute)
+app.use(userMessagesRoute)
 
 app.use(errorHandler)
 
