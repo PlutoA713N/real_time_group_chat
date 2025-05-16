bef7f6f92eab92afa118ac5188092aced3d39ec7
PlutoA713N <mallachandu99@gmail.com>
2025-05-17 02:34:19 +0530

    feat: Validate function is added to validate the document

--- src/models/dbOperations.ts
+++ src/models/dbOperations.ts
@@ -1,5 +1,8 @@
-import { UserRegistrationModel } from "./user.model";
+import {Model} from "mongoose";
 import { AppError } from "./../errors/AppError";
+import {NextFunction, Request} from "express";
+import {createError} from "../errors/createError";
+import {IAutenticatedRequest} from "../middleware/checkidHandler";
 
 interface CheckResult <T> {
     message: string,
@@ -7,9 +10,9 @@ interface CheckResult <T> {
     isExists: boolean
 }
 
-export async function checkFieldExists<T = any>(field: string, value: string): Promise<CheckResult<T>> {
+export async function checkFieldExists<T = any>(model: Model<any>, field: string, value: string): Promise<CheckResult<T>> {
     try {
-        const existingUser = await UserRegistrationModel.findOne({ [field]: value }) 
+        const existingUser = await model.findOne({ [field]: value })
         if (existingUser) {
             return { message: `${[field]} exists`, user: existingUser as T, isExists: true }
         }
@@ -21,5 +24,15 @@ export async function checkFieldExists<T = any>(field: string, value: string): P
             'DB_CHECK_ERROR'
         );
     }
+}
+
+export async function validateAndAttach(model: Model<any>,fieldId: string, field: string, req: IAutenticatedRequest) {
+    const result = await checkFieldExists(model,'_id', fieldId);
+    if (!result.isExists) {
+        throw createError(`${field} does not exist`, 404, `${field.toUpperCase()}_NOT_FOUND`);
+    }
+
+    req[field] = result.user;
+    return req
 }
 
