0bd3acb792019aa5b3b8df7fc2524dfb7b048593
PlutoA713N <mallachandu99@gmail.com>
2025-05-17 02:33:24 +0530

    refactor: Use the db function to validate the receiver, group

--- src/middleware/checkidHandler.ts
+++ src/middleware/checkidHandler.ts
@@ -1,17 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import {createError} from "./../errors/createError";
-import {checkFieldExists} from "../models/dbOperations";
-import {IUserRegistration, IUser} from "../models/user.model";
-import {IGroup} from "../models/group.model";
+import {validateAndAttach} from "../models/dbOperations";
+import {IUserRegistration, IUser, UserRegistrationModel} from "../models/user.model";
+import {IGroup, GroupModel} from "../models/group.model";
 
 export interface IAutenticatedRequest extends Request {
     user?: IUser;
     receiverUser?: IUserRegistration;
     group?: IGroup;
+    [key: string]: any;
 }
 
 export const checkidHandler = async (req: IAutenticatedRequest, res:Response, next:NextFunction) => {
-    const {senderId, receiverId = '', groupId = ''} = req.body
+    const {senderId, receiverId = '', withUserId = '', groupId = ''} = req.body
     const user = req.user
 
     // verify the senderId user
@@ -23,28 +24,20 @@ export const checkidHandler = async (req: IAutenticatedRequest, res:Response, ne
         return next(createError('Sender ID does not match the authenticated user', 403, 'INVALID_USER'))
     }
 
-
-    // verify the receiverId
-    if(receiverId) {
-        const receiverUser = await checkFieldExists('_id', receiverId)
-
-        if(!receiverUser.isExists) {
-            return next(createError('Receiver ID does not exist', 404, 'USER_NOT_FOUND'))
+    try{
+        if(receiverId) {
+            await  validateAndAttach(UserRegistrationModel, receiverId, 'receiverUser', req)
+        }
+        if(withUserId) {
+            await  validateAndAttach(UserRegistrationModel, withUserId, 'receiverUser', req)
+        }
+        if(groupId) {
+            await  validateAndAttach(GroupModel, groupId, 'group', req)
         }
 
-        req.receiverUser = receiverUser.user
+        return next()
+    }catch(error){
+        return next(error)
     }
 
-    // verify the group
-    if(groupId){
-        const group = await checkFieldExists('_id', groupId)
-
-        if(!group.isExists) {
-            return next(createError('Group ID does not exist', 404, 'GROUP_NOT_FOUND'))
-        }
-
-        req.group = group.user
-    }
-
-    return next()
 }
