480696ad7ae512dbf9ae7b10120c752478b88b8a
PlutoA713N <mallachandu99@gmail.com>
2025-05-17 02:42:46 +0530

    fix: Add model in the function params

--- src/utils/validation.utils.ts
+++ src/utils/validation.utils.ts
@@ -1,9 +1,10 @@
 import { checkFieldExists } from "../models/dbOperations";
+import { UserRegistrationModel } from "../models/user.model";
 
 export const findUserByField = async (form: object) => {
     for (const [field, value] of Object.entries(form)) {
         if (value) {
-            const result = await checkFieldExists(field, value)
+            const result = await checkFieldExists(UserRegistrationModel, field, value)
             if (result.isExists && result.user) {
                 return result.user
             }
