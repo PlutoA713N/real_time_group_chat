59c04e4809ac914a9c3238659f853f9bd4e84bef
PlutoA713N <mallachandu99@gmail.com>
2025-05-19 04:33:02 +0530

    feat: Add a new creatorId property

--- src/models/group.model.ts
+++ src/models/group.model.ts
@@ -3,11 +3,13 @@ import { model, Schema } from "mongoose";
 export interface IGroup {
     name: string;
     members: [string];
+    creatorId: string;
 }
 
 const GroupSchema = new Schema({
     name: { type: String, required: true },
-    members: [{ type: String, required: true }]
+    members: [{ type: String, required: true }],
+    creatorId: { type: String, required: true }
 }, { timestamps: true })
 
 
