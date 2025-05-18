bfb3ce096a8d01fbff3396c71ae36bbe12115504
PlutoA713N <mallachandu99@gmail.com>
2025-05-19 04:30:27 +0530

    refactor: Update the interface name

--- src/middleware/authenticationHandler.ts
+++ src/middleware/authenticationHandler.ts
@@ -3,19 +3,19 @@ import { createError } from "./../errors/createError";
 import { IDecodedToken, verifyJwtToken } from "./../utils/jwt";
 import {checkFieldExists, validateAndAttach} from "./../models/dbOperations";
 import { UserRegistrationModel } from "./../models/user.model";
-import {IAutenticatedRequest} from "./checkidHandler";
+import {IAuthenticatedRequest} from "./checkidHandler";
 
 export const authenticationHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const token = req.headers.authorization?.split(' ')[1]
 
         if (!token) {
-            return next(createError('Missing token', 400, 'INVALID_TOKEN'))
+            return next(createError('Missing token', 400, 'INVALID_AUTH_TOKEN'))
         }
 
         const { userId } = verifyJwtToken(token) as IDecodedToken
 
-        await validateAndAttach(UserRegistrationModel, userId, 'user', req as IAutenticatedRequest)
+        await validateAndAttach(UserRegistrationModel, userId, 'user', req as IAuthenticatedRequest)
 
         return next()
 
