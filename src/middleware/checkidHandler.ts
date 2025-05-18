1ad4aa35fd1feeda6196b652a07dd5ac24c75a15
PlutoA713N <mallachandu99@gmail.com>
2025-05-19 04:31:28 +0530

    feat: Add a new validateGroupMessagesRequest, also update the source into array

--- src/middleware/checkidHandler.ts
+++ src/middleware/checkidHandler.ts
@@ -4,40 +4,80 @@ import {validateAndAttach} from "../models/dbOperations";
 import {IUserRegistration, IUser, UserRegistrationModel} from "../models/user.model";
 import {IGroup, GroupModel} from "../models/group.model";
 
-export interface IAutenticatedRequest extends Request {
+export interface IAuthenticatedRequest extends Request {
     user?: IUser;
     receiverUser?: IUserRegistration;
     group?: IGroup;
     [key: string]: any;
 }
 
-export const checkidHandler = async (req: IAutenticatedRequest, res:Response, next:NextFunction) => {
-    const {senderId, receiverId = '', withUserId = '', groupId = ''} = req.body
-    const user = req.user
+export const validateIds = async (
+    req: IAuthenticatedRequest,
+    res: Response,
+    next: NextFunction,
+    source: ('body' | 'query' | 'params')[]
+) => {
 
-    // verify the senderId user
-    if(!user) {
-        return next(createError('User not authenticated', 401, 'UNAUTHORIZED'))
-    }
+    const user = req.user;
 
-    if(user._id.toString()!== senderId) {
-        return next(createError('Sender ID does not match the authenticated user', 403, 'INVALID_USER'))
+    if (!user) {
+        return next(createError('User not authenticated', 401, 'UNAUTHORIZED'));
     }
 
     try{
-        if(receiverId) {
-            await  validateAndAttach(UserRegistrationModel, receiverId, 'receiverUser', req)
-        }
-        if(withUserId) {
-            await  validateAndAttach(UserRegistrationModel, withUserId, 'receiverUser', req)
-        }
-        if(groupId) {
-            await  validateAndAttach(GroupModel, groupId, 'group', req)
-        }
+        for(const type of source) {
+            const data = req[type] || {};
+            const {
+                senderId,
+                userId,
+                receiverId,
+                withUserId,
+                groupId
+            } = data;
 
-        return next()
-    }catch(error){
-        return next(error)
+            const authId = senderId || userId;
+
+            if (user._id.toString() !== authId) {
+                return next(createError('Sender/User ID does not match the authenticated user', 403, 'INVALID_USER'));
+            }
+
+            if (receiverId) {
+                await validateAndAttach(UserRegistrationModel, receiverId, 'receiverUser', req);
+            }
+            if (withUserId) {
+                await validateAndAttach(UserRegistrationModel, withUserId, 'receiverUser', req);
+            }
+            if (groupId) {
+                await validateAndAttach(GroupModel, groupId, 'group', req);
+            }
+        }
+        return next();
+    } catch (err) {
+        return next(err);
     }
+};
 
+
+export const checkMessageIdsFromBody = (
+    req: IAuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    return validateIds(req, res, next, ['body']);
+};
+
+export const checkHistoryIdsFromQuery = (
+    req: IAuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+    return validateIds(req, res, next, ['query']);
+};
+
+export const validateGroupMessagesRequest = (
+    req: IAuthenticatedRequest,
+    res: Response,
+    next: NextFunction
+): Promise<void> => {
+     return validateIds(req, res, next, ['params', 'body']);
 }
