b8a3bbaa62b20c55919563f33ac03a4bdb8a4a56
PlutoA713N <mallachandu99@gmail.com>
2025-05-21 22:51:51 +0530

    perf: validate token using redis

--- src/middleware/authenticationHandler.ts
+++ src/middleware/authenticationHandler.ts
@@ -4,9 +4,16 @@ import { IDecodedToken, verifyJwtToken } from "./../utils/jwt";
 import {checkFieldExists, validateAndAttach} from "./../models/dbOperations";
 import { UserRegistrationModel } from "./../models/user.model";
 import {IAuthenticatedRequest} from "./checkidHandler";
+import {getUserToken} from "../redis/redisUtils";
 
 export const authenticationHandler = async (req: Request, res: Response, next: NextFunction) => {
     try {
+
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return next(createError('Missing or invalid token', 401, 'INVALID_AUTH_TOKEN'));
+        }
+
         const token = req.headers.authorization?.split(' ')[1]
 
         if (!token) {
@@ -14,6 +21,15 @@ export const authenticationHandler = async (req: Request, res: Response, next: N
         }
 
         const { userId } = verifyJwtToken(token) as IDecodedToken
+        const storedTokenValue = await getUserToken(userId)
+
+        if (!storedTokenValue) {
+            return next(createError('Token not found (likely expired)', 401, 'EXPIRED_AUTH_TOKEN'));
+        }
+
+        if (storedTokenValue !== token) {
+            return next(createError('Token has been revoked or replaced', 401, 'EXPIRED_AUTH_TOKEN'));
+        }
 
         await validateAndAttach(UserRegistrationModel, userId, 'user', req as IAuthenticatedRequest)
 
