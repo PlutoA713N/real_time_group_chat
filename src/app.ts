6fd916b46ab45c4c7430e1302bea7b61d17d4eaf
PlutoA713N <mallachandu99@gmail.com>
2025-05-21 22:48:50 +0530

    feat: import redis setup functions

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 // app.ts
 import { run, closeDB } from "./config/database";
 import app from "./express/index";
-import { setupSocketIO } from "./sockets/index"; // 👈 renamed import
+import { setupSocketIO } from "./sockets/index";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import { setSocketIOInstance } from "./sockets/io"
-import { log, error } from "console";
-import { exit } from "process";
 import { getEnv } from "./config/env";
+import {initRedis, closeRedis} from "./redis/redisClient";
+import { exit } from "process";
+import { log, error } from "console";
+
 
 const port = Number(getEnv("PORT", true, "5000"));
 
@@ -19,24 +21,26 @@ const io = new Server(httpServer, {
     }
 });
 setupSocketIO(io);
-setSocketIOInstance(io)// ✅ Setup your socket logic
+setSocketIOInstance(io)
 
 const startServer = async () => {
     try {
         await run();
+        await initRedis();
         httpServer.listen(port, () => {
             log("Server is running on PORT:", port);
         });
     } catch (err) {
-        error("Failed to connect mongodb or start server:", err);
+        error("Failed to connect mongodb/Redis or start server:", err);
         exit(1);
     }
 };
 
 process.on("SIGTERM", async () => {
-    log("Recieved SIGTERM signal, Closing MongoDB connnection...");
+    log("Recieved SIGTERM signal, Closing MongoDB, Redis connnection...");
     try {
         await closeDB();
+        await closeRedis();
         log("MongoDB connection closed");
     } catch (err) {
         error("Error closing MongoDB connection:", err);
